657469fd62f0b3bee6d0bad7ebd8f42581cc8ce5
Fast-Dev-Kit <2642754767@qq.com>
2015-08-25 11:45:14 +0800

    增加一个UUID工具类

---
+++ x-common/src/main/java/net/datafans/common/util/UUID.java
@@ -0,0 +1,12 @@
+package net.datafans.common.util;
+
+import org.apache.commons.lang.math.RandomUtils;
+
+public class UUID {
+
+	public static long getMsgID() {
+		long ts = System.currentTimeMillis();
+		int random = RandomUtils.nextInt(999999 - 100000 + 1) + 100000;
+		return Long.parseLong(new StringBuilder(String.valueOf(ts).substring(1)).append(random).toString());
+	}
+}
